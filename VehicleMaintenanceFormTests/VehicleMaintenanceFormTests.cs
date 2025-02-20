e7bed58477c27121e58142850d99a8a5495b0eff
sreyas96 <sreyassaji888@gmail.com>
2025-02-20 16:41:51 -0500

    Modified Test Units

--- VehicleMaintenanceFormTests/VehicleMaintenanceFormTests.cs
+++ VehicleMaintenanceFormTests/VehicleMaintenanceFormTests.cs
@@ -9,7 +9,7 @@ namespace VehicleMaintenanceFormTests
         [Test]
         public void TestModel_ValidInput_ReturnsCorrectModel()
         {
-            var vehicle = new Vehicle("Toyota Corola", 2022, 50000);
+            var vehicle = new Vehicle("Toyota Corola", 2022, 42265);
             var result = vehicle.GetModel();
             Assert.That(result, Is.EqualTo("Toyota Corola"));
         }
@@ -17,7 +17,7 @@ namespace VehicleMaintenanceFormTests
         [Test]
         public void TestYear_ValidInput_ReturnsCorrectYear()
         {
-            var vehicle = new Vehicle("Honda Civic", 2022, 30000);
+            var vehicle = new Vehicle("Honda Civic", 2022, 98235);
             var result = vehicle.GetYear();
             Assert.That(result, Is.EqualTo(2022));
         }
@@ -25,15 +25,15 @@ namespace VehicleMaintenanceFormTests
         [Test]
         public void TestKilometers_ValidInput_ReturnsCorrectKilometers()
         {
-            var vehicle = new Vehicle("Ford Explorer", 2022, 75000);
+            var vehicle = new Vehicle("Ford Explorer", 2022, 51899);
             var result = vehicle.GetKilometers();
-            Assert.That(result, Is.EqualTo(75000));
+            Assert.That(result, Is.EqualTo(51899));
         }
 
         [Test]
         public void TestModel_MaxLength_DoesNotExceedLimit()
         {
-            var vehicle = new Vehicle("Ford Forunner", 2022, 10000);
+            var vehicle = new Vehicle("Ford Forunner", 2022, 25025);
             var result = vehicle.GetModel();
             Assert.That(result.Length, Is.LessThanOrEqualTo(15));
         }
