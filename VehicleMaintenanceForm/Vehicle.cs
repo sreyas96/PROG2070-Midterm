e2d424e4c4edf068d2ca89b821c331b87abf58da
sreyas96 <sreyassaji888@gmail.com>
2025-02-20 16:24:23 -0500

    Initial commit

---
+++ VehicleMaintenanceForm/Vehicle.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Vehicle.cs  
+using System;
+
+namespace VehicleMaintenanceForm
+{
+    public class Vehicle
+    //BVA Ranges
+    //Model = Maximum 15 characters
+    //Year = 1990-2025
+    //Kilometers = 0-250000
+    {
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public int Kilometers { get; set; }
+
+        public Vehicle(string model, int year, int kilometers)
+        {
+            Model = model;
+            Year = year;
+            Kilometers = kilometers;
+        }
+
+        public string GetModel()
+        {
+            return Model;
+        }
+
+        public int GetYear()
+        {
+            return Year;
+        }
+
+        public int GetKilometers()
+        {
+            return Kilometers;
+        }
+    }
+}
---
+++ VehicleMaintenanceFormTests/VehicleMaintenanceFormTests.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using VehicleMaintenanceForm;
+
+namespace VehicleMaintenanceFormTests
+{
+    [TestFixture]
+    public class VehicleTests
+    {
+        [Test]
+        public void TestModel_ValidInput_ReturnsCorrectModel()
+        {
+            var vehicle = new Vehicle("Toyota Corola", 2022, 50000);
+            var result = vehicle.GetModel();
+            Assert.That(result, Is.EqualTo("Toyota Corola"));
+        }
+
+        [Test]
+        public void TestYear_ValidInput_ReturnsCorrectYear()
+        {
+            var vehicle = new Vehicle("Honda Civic", 2022, 30000);
+            var result = vehicle.GetYear();
+            Assert.That(result, Is.EqualTo(2022));
+        }
+
+        [Test]
+        public void TestKilometers_ValidInput_ReturnsCorrectKilometers()
+        {
+            var vehicle = new Vehicle("Ford Explorer", 2022, 75000);
+            var result = vehicle.GetKilometers();
+            Assert.That(result, Is.EqualTo(75000));
+        }
+
+        [Test]
+        public void TestModel_MaxLength_DoesNotExceedLimit()
+        {
+            var vehicle = new Vehicle("Ford Forunner", 2022, 10000);
+            var result = vehicle.GetModel();
+            Assert.That(result.Length, Is.LessThanOrEqualTo(15));
+        }
+
+        [Test]
+        public void TestYear_ValidRange_IsWithinBounds()
+        {
+            var vehicle = new Vehicle("Hundai Elantra", 2022, 5000);
+            var result = vehicle.GetYear();
+            Assert.That(result, Is.InRange(1990, 2025));
+        }
+
+        
+    }
+}
